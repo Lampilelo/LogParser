4311b776b0882d17e2c78104e36b65e5ee531523
Lampilelo <cubex7@gmail.com>
2016-10-15 17:33:42 +0200

    Added some info.

--- src/LogParser/Properties/AssemblyInfo.cs
+++ src/LogParser/Properties/AssemblyInfo.cs
@@ -1,12 +1,11 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyConfiguration("Debug")]
+[assembly: AssemblyCompany("RH Tech")]
 [assembly: AssemblyProduct("LogParser")]
 [assembly: AssemblyTrademark("")]
 
