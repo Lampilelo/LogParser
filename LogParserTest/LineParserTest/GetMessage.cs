c6a829de59824606d04a53d1966173779b0df515
Lampilelo <cubex7@gmail.com>
2016-11-21 11:36:09 +0100

    Added Tests project to solution

---
+++ LogParserTest/LineParserTest/GetMessage.cs
@@ -0,0 +1,110 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LogParser;
+using System;
+
+public partial class LineParserTest
+{
+    [TestMethod]
+    public void Test_GetType_Whisper()
+    {
+        MessageType type1 = LineParser.GetType("To SomeDrek-MeRealm: this is. some,/ messAGE*>12$");
+        MessageType type2 = LineParser.GetType("To SomeDrek: this is. some,/ messAGE*>12$");
+        MessageType type3 = LineParser.GetType("SomeDrek-MeRealm whispers: może stad że wszyscy inni sa");
+        MessageType type4 = LineParser.GetType("SomeDrek whispers: może stąd że wszyscy inni sa ęółśćźżń");
+
+        Assert.AreEqual(MessageType.Whisper, type1);
+        Assert.AreEqual(MessageType.Whisper, type2);
+        Assert.AreEqual(MessageType.Whisper, type3);
+        Assert.AreEqual(MessageType.Whisper, type4);
+    }
+
+    [TestMethod]
+    public void Test_GetType_Raid()
+    {
+        Assert.Fail();
+    }
+
+    [TestMethod]
+    public void Test_GetType_Party()
+    {
+        Assert.Fail();
+    }
+
+    [TestMethod]
+    public void Test_GetType_Say()
+    {
+        Assert.Fail();
+    }
+
+    [TestMethod]
+    public void Test_GetType_Yell()
+    {
+        Assert.Fail();
+    }
+
+    [TestMethod]
+    public void Test_GetType_Officer()
+    {
+        Assert.Fail();
+    }
+
+    [TestMethod]
+    public void Test_GetType_Guild()
+    {
+        Assert.Fail();
+    }
+
+    [TestMethod]
+    public void Test_GetType_Loot()
+    {
+        Assert.Fail();
+    }
+
+    [TestMethod]
+    public void Test_GetType_Achievement()
+    {
+        Assert.Fail();
+    }
+
+    [TestMethod]
+    public void Test_GetType_System()
+    {
+        Assert.Fail();
+    }
+
+    [TestMethod]
+    public void Test_GetType_Instance()
+    {
+        Assert.Fail();
+    }
+
+    [TestMethod]
+    public void Test_GetType_CommandOutput()
+    {
+        Assert.Fail();
+    }
+
+    [TestMethod]
+    public void Test_GetType_Roll()
+    {
+        Assert.Fail();
+    }
+
+    [TestMethod]
+    public void Test_GetType_GenericChat()
+    {
+        Assert.Fail();
+    }
+
+    [TestMethod]
+    public void Test_GetType_GeneralChat()
+    {
+        Assert.Fail();
+    }
+
+    [TestMethod]
+    public void Test_GetType_CustomChat()
+    {
+        Assert.Fail();
+    }
+}
---
+++ LogParserTest/LineParserTest/GetTimeFromStringTest.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LogParser;
+using System;
+
+[TestClass]
+public partial class LineParserTest
+{
+    [TestMethod]
+    public void Test_GetTimeFromString_ReturnsDefault_OnNonCompatibleString()
+    {
+        DateTime dateTime1 = LineParser.GetTimeFromString("");
+        DateTime dateTime2 = LineParser.GetTimeFromString(null);
+        DateTime dateTime3 = LineParser.GetTimeFromString("blablabla");
+        DateTime dateTime4 = LineParser.GetTimeFromString("32423423");
+        DateTime dateTime5 = LineParser.GetTimeFromString("g1");
+        DateTime dateTime6 = LineParser.GetTimeFromString("234 sdf 23");
+
+        Assert.AreEqual(default(DateTime), dateTime1);
+        Assert.AreEqual(default(DateTime), dateTime2);
+        Assert.AreEqual(default(DateTime), dateTime3);
+        Assert.AreEqual(default(DateTime), dateTime4);
+        Assert.AreEqual(default(DateTime), dateTime5);
+        Assert.AreEqual(default(DateTime), dateTime6);
+    }
+
+   
+}
