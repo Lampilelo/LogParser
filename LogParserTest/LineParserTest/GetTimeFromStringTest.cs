cff209f7167f4d5d471d08bd97d5566f996438f1
Lampilelo <cubex7@gmail.com>
2016-11-24 18:48:14 +0100

    No longer valid. Left for reference.

--- LogParserTest/LineParserTest/GetTimeFromStringTest.cs
+++ LogParserTest/LineParserTest/GetTimeFromStringTest.cs
@@ -8,19 +8,14 @@ public partial class LineParserTest
     [TestMethod]
     public void Test_GetTimeFromString_ReturnsDefault_OnNonCompatibleString()
     {
-        DateTime dateTime1 = LineParser.GetTimeFromString("");
-        DateTime dateTime2 = LineParser.GetTimeFromString(null);
-        DateTime dateTime3 = LineParser.GetTimeFromString("blablabla");
-        DateTime dateTime4 = LineParser.GetTimeFromString("32423423");
-        DateTime dateTime5 = LineParser.GetTimeFromString("g1");
-        DateTime dateTime6 = LineParser.GetTimeFromString("234 sdf 23");
+        Line line1 = LineParser.ParseLine("");
+        Line line2 = LineParser.ParseLine(null);
+        Line line3 = LineParser.ParseLine("blablabla");
+        Line line4 = LineParser.ParseLine("32423423");
+        Line line5 = LineParser.ParseLine("g1");
+        Line line6 = LineParser.ParseLine("234 sdf 23");
 
-        Assert.AreEqual(default(DateTime), dateTime1);
-        Assert.AreEqual(default(DateTime), dateTime2);
-        Assert.AreEqual(default(DateTime), dateTime3);
-        Assert.AreEqual(default(DateTime), dateTime4);
-        Assert.AreEqual(default(DateTime), dateTime5);
-        Assert.AreEqual(default(DateTime), dateTime6);
+        Assert.Fail();
     }
 
    
