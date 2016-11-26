922b37d29fcb66dc3fea825f44fc7092040f65a0
Lampilelo <cubex7@gmail.com>
2016-11-26 20:05:41 +0100

    Added Name property

--- src/LogParser/Line.cs
+++ src/LogParser/Line.cs
@@ -4,20 +4,21 @@ namespace LogParser
 {
     /// <summary>
     /// Class for storing information of log line.
-    /// It's not supposed to have real complex informations, 
-    /// just time, type and string text value.
     /// </summary>
     public class Line
     {
+        //TODO: Maybe add Realm? In that case Name shouldn't contain realm name
         public DateTime Time { get; set; }
         public MessageType Type { get; set; }
         public string Text { get; set; }
+        public string Name { get; set; }
 
-        public Line(DateTime time, MessageType type, string text)
+        public Line(DateTime time, MessageType type, string text, string name = null)
         {
             Time = time;
             Type = type;
             Text = text;
+            Name = name;
         }
 
         public Line()
@@ -25,6 +26,7 @@ namespace LogParser
             Time = new DateTime();
             Type = MessageType.NotDefined;
             Text = null;
+            Name = null;
         }
     }
 }
