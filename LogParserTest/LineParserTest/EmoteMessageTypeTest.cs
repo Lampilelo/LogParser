d0da69df7fe10b7ee14e40e51972d70b12e48e41
Lampilelo <cubex7@gmail.com>
2016-11-28 13:41:33 +0100

    Added test and refactored old ones

--- LogParserTest/LineParserTest/EmoteMessageTypeTest.cs
+++ LogParserTest/LineParserTest/EmoteMessageTypeTest.cs
@@ -8,6 +8,7 @@ public partial class LineParserTest
     [TestMethod]
     public void Test_GetType_NotDefinedOrSystem_BeforeParseEmote()
     {
+        LineParser.ClearNameSet();
         Line line1 = LineParser.ParseLine("10/29 16:50:22.515  Finky podrapal sie po glowie.");
         Line line2 = LineParser.ParseLine("11/7 16:54:10.281  Nelwo has gone offline.");
         Line line3 = LineParser.ParseLine("11/7 16:54:30.812  Nelwo has come online.");
@@ -22,6 +23,7 @@ public partial class LineParserTest
     [TestMethod]
     public void Test_GetType_Emote()
     {
+        LineParser.ClearNameSet();
         // Populate nameSet with data
         LineParser.ParseLine("8/29 09:34:49.812  Finky says: I pare lat by nam zajelo wydobycie.");
         LineParser.ParseLine("2/1 15:20:04.045  Nelwo whispers: questy sie robi");
@@ -35,10 +37,11 @@ public partial class LineParserTest
         Line line3 = LineParser.ParseLine("11/7 16:54:30.812  Nelwo has come online.");
         Line line4 = LineParser.ParseLine("5/6 23:29:51.792  Nelwo zaczal poprawiac siodlo");
 
-        LineParser.ParseEmote(ref line1);
-        LineParser.ParseEmote(ref line2);
-        LineParser.ParseEmote(ref line3);
-        LineParser.ParseEmote(ref line4);
+        LineParser.PrepareEmoteParsing();
+        line1 = LineParser.ParseEmote(line1);
+        line2 = LineParser.ParseEmote(line2);
+        line3 = LineParser.ParseEmote(line3);
+        line4 = LineParser.ParseEmote(line4);
 
         Assert.AreEqual(MessageType.Emote, line1.Type);
         Assert.AreEqual(MessageType.System, line2.Type);
@@ -49,6 +52,7 @@ public partial class LineParserTest
     [TestMethod]
     public void Test_GetName_Emote()
     {
+        LineParser.ClearNameSet();
         // Fill nameSet with data
         LineParser.ParseLine("8/29 09:34:49.812  Finky says: I pare lat by nam zajelo wydobycie.");
         LineParser.ParseLine("2/1 15:20:04.045  Nelwo whispers: questy sie robi");
@@ -56,21 +60,33 @@ public partial class LineParserTest
         LineParser.ParseLine("10/8 10:28:17.225  [2. Trade] Fuztail: just write no manlets allowed ");
         LineParser.ParseLine("10/8 15:25:11.146  |Hchannel:GUILD|h[Guild]|h Swistaq-ArgentDawn: jest jakas meta/");
         LineParser.ParseLine("10/10 19:44:42.366  You receive loot: Spaulders of Kor'kron Fealty.");
-        
+
         Line line1 = LineParser.ParseLine("10/29 16:50:22.515  Finky podrapal sie po glowie.");
         Line line2 = LineParser.ParseLine("11/7 16:54:10.281  Nelwo has gone offline.");
         Line line3 = LineParser.ParseLine("11/7 16:54:30.812  Nelwo has come online.");
         Line line4 = LineParser.ParseLine("5/6 23:29:51.792  Nelwo zaczal poprawiac siodlo");
 
-        LineParser.ParseEmote(ref line1);
-        LineParser.ParseEmote(ref line2);
-        LineParser.ParseEmote(ref line3);
-        LineParser.ParseEmote(ref line4);
+        LineParser.PrepareEmoteParsing();
+        line1 = LineParser.ParseEmote(line1);
+        line2 = LineParser.ParseEmote(line2);
+        line3 = LineParser.ParseEmote(line3);
+        line4 = LineParser.ParseEmote(line4);
 
         Assert.AreEqual(line1.Name, "Finky");
         Assert.AreEqual(line2.Name, null);
         Assert.AreEqual(line3.Name, null);
         Assert.AreEqual(line4.Name, "Nelwo");
+    }
+
+    [TestMethod]
+    public void Test_Emote_ThrowsException_OnNotPrepareEmoteParsing()
+    {
+        LineParser.ClearNameSet();
+        // Fill nameSet with data
+        LineParser.ParseLine("8/29 09:34:49.812  Finky says: I pare lat by nam zajelo wydobycie.");
+
+        Line line1 = LineParser.ParseLine("10/29 16:50:22.515  Finky podrapal sie po glowie.");
+        Assert.ThrowsException<Exception>( () => LineParser.ParseEmote(line1));
     }
 }
 
